c9ac3e57f28c8d0ad7dcd067f9de40c0f28c1ece
iqbaldiit <iqbaldiit@gmail.com>
2025-01-20 15:04:12 +0600

    crud of Pets

---
+++ src/Wpm.Management.Api/Controllers/BreedController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using Wpm.Management.Api.DataAccess;
+
+namespace Wpm.Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreedController(ManagementDbContext dbContext, ILogger<BreedController> logger) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Gets()
+        {
+            var oBreeds= await dbContext.Breeds.ToListAsync();
+            return oBreeds != null ? Ok(oBreeds) : NotFound();
+        }
+
+        [HttpGet("{id}", Name =nameof(GetBreedById))]
+        public async Task<IActionResult> GetBreedById(int id)
+        {
+            var oBreed = await dbContext.Breeds.SingleOrDefaultAsync(b => b.Id == id);
+            return oBreed != null ? Ok(oBreed) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(NewBreed oNewBreed)
+        {
+            try
+            {
+                var oBreed = oNewBreed.ToBreed();
+                await dbContext.AddRangeAsync(oBreed);
+                await dbContext.SaveChangesAsync();
+                return CreatedAtRoute(nameof(GetBreedById), new { id = oBreed.Id }, oNewBreed);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.ToString());
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+    }
+
+    public record NewBreed(string Name)
+    {
+        public Breed ToBreed()
+        {
+            return new Breed(0,Name);
+        }
+    }
+}
--- src/Wpm.Management.Api/Controllers/PetsController.cs
+++ src/Wpm.Management.Api/Controllers/PetsController.cs
@@ -1,19 +1,59 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 using Wpm.Management.Api.DataAccess;
 
 namespace Wpm.Management.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PetsController(ManagementDbContext dbContext) : ControllerBase
+    public class PetsController(ManagementDbContext dbContext, ILogger<PetsController> logger) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             var all = await dbContext.Pets.Include(p => p.Breed).ToListAsync();
             return Ok(all);
+        }
+
+        [HttpGet("{id}", Name =nameof(GetById))]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var oPet = await dbContext.Pets.Include(p=>p.Breed)
+                .Where(p=>p.Id==id)
+                .FirstOrDefaultAsync();
+
+            return Ok(oPet);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(NewPet oNewPet)
+        {
+            try
+            {
+                var oPet = oNewPet.ToPet();
+                await dbContext.AddRangeAsync(oPet);
+                await dbContext.SaveChangesAsync();
+                return CreatedAtRoute(nameof(GetById), new { id = oPet.Id }, oNewPet);
+            }catch (Exception ex)
+            {
+                logger.LogError(ex.Message.ToString());
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+    }
+
+    public record NewPet(string Name, int Age, int BreedId)
+    {
+        public Pet ToPet()
+        {
+            return new Pet()
+            {
+                Name = Name,
+                Age = Age,
+                BreedId = BreedId
+            };
         }
     }
 }
